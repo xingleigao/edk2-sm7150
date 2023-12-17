3a7277a74f913d5298421f141bbd0dbe0efc14a1
xingleigao <xingleigao@qq.com>
2023-12-17 22:52:55 +0800

    add lenove xiaoxin pro20 & fix CROSS BUILD

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 # based on the instructions from edk2-platform
 set -e
 . build_common.sh
-PYTHON_COMMAND=python2.7
+PYTHON_COMMAND=python
 
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX= build -s -n 0 -a AARCH64 -t GCC5 -p F11/F11.dsc
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p F11/F11.dsc
 
 gzip -c < Build/F11/DEBUG_GCC5/FV/F11_UEFI.fd >uefi_image
 
